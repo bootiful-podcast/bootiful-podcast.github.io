25635e56ada1ba0baa7915372874529ee29c9205
Josh Long <josh@joshlong.com>
2020-02-04 01:18:28 -0800

    adding assets @ 2020-02-04T09:18:28.044201Z

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -50,6 +50,7 @@ function buildMetaArtistHtml(title) {
 }
 
 function initializePlayerForLatest(podcast) {
+    $('#'  + containerOfDataSources).show();
     console.log('initializing the latest with the episode information');
     var playerNode = jQuery(playerId);
     playerNode.attr('data-source', podcast.episodeUri);
